52cc4d6402112b6cea1dcb4dceff3766cd98a841
Aiden Nelson <aidenjn@gmail.com>
2025-11-21 08:31:42 -0800

    Added bounce in for page headings.

--- site/components/PageHeading.tsx
+++ site/components/PageHeading.tsx
@@ -1,23 +1,61 @@
+"use client";
+
 import { CustomIcon } from "@/lib/types/CustomIcon";
 import CustomIconSVG from "./CustomIconSVG";
+import { motion } from "framer-motion";
 
-export default function PageHeading(
-  {
-    titleText,
-    descriptionText,
-    icon,
-  }: {
-    titleText: string,
-    descriptionText?: string,
-    icon?: CustomIcon
-  }
-) {
+export default function PageHeading({
+  titleText,
+  descriptionText,
+  icon,
+}: {
+  titleText: string;
+  descriptionText?: string;
+  icon?: CustomIcon;
+}) {
+  // Variants for staggered animation
+  const containerVariants = {
+    hidden: {},
+    visible: {
+      transition: {
+        staggerChildren: 0.15,
+      },
+    },
+  };
+
+  const itemVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0, transition: { duration: 0.6 } },
+  };
+
   return (
-    <div className="max-w-5xl mx-auto pr-6 pl-6 max-w-200">
-      <h1 className="text-3xl font-semibold text-foreground text-center">{ icon && <CustomIconSVG icon={ icon } className="w-10 h-10 sm:w-15 sm:h-15 mr-5 stroke-white inline-block mr-5" />}{ titleText }</h1>
-      { descriptionText && (
-        <p className="text-left mt-4 mb-4">{ descriptionText }</p>
-      ) }
-    </div>
+    <motion.div
+      className="max-w-5xl mx-auto px-6 text-center"
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+    >
+      <motion.div
+        className="flex items-center justify-center gap-4 mb-3 inline-flex"
+        variants={itemVariants}
+      >
+        {icon && (
+          <CustomIconSVG
+            icon={icon}
+            className="w-10 h-10 sm:w-15 sm:h-15 stroke-white"
+          />
+        )}
+        <h1 className="text-3xl font-semibold text-foreground">{titleText}</h1>
+      </motion.div>
+
+      {descriptionText && (
+        <motion.p
+          variants={itemVariants}
+          className="mt-4 text-foreground max-w-2xl mx-auto"
+        >
+          {descriptionText}
+        </motion.p>
+      )}
+    </motion.div>
   );
 }
